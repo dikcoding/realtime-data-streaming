f2333c8e2c39a31862feefed571d10748c23084f
dikcoding <andikafadillasiagian0@gmail.com>
2025-05-07 05:51:39 +0700

    feat: Add Airflow DAG for user data streaming to Kafka

--- dags/kafka_stream.py
+++ dags/kafka_stream.py
@@ -1,10 +1,72 @@
-from datatime import datatime
+import uuid
+from datetime import datetime
 from airflow import DAG
-from airflow.operators import PythonOperator
+from airflow.operators.python import PythonOperator
 
+# konfigurasi DAG
 default_args = {
     'owner': 'airscholar',
-    'start_date': datatime(2025, 5, 7, 10, 00)
+    'start_date': datetime(2025, 5, 7, 10, 00)
 }
 
-with DAG()
+def get_data():
+    import requests
+
+    res = requests.get("https://randomuser.me/api/")
+    res = res.json()
+    res = res['results'][0]
+
+    return res
+
+def format_data(res):
+    data = {}
+    location = res['location']
+    data['id'] = uuid.uuid4()
+    data['first_name'] = res['name']['first']
+    data['last_name'] = res['name']['last']
+    data['gender'] = res['gender']
+    data['address'] = f"{str(location['street']['number'])} {location['street']['name']}, " \
+                      f"{location['city']}, {location['state']}, {location['country']}"
+    data['post_code'] = location['postcode']
+    data['email'] = res['email']
+    data['username'] = res['login']['username']
+    data['dob'] = res['dob']['date']
+    data['registered_date'] = res['registered']['date']
+    data['phone'] = res['phone']
+    data['picture'] = res['picture']['medium']
+
+    return data
+
+def stream_data():
+    import json
+    from kafka import KafkaProducer
+    import time
+    import logging
+
+    # menghubungkan ke broker kafka
+    producer = KafkaProducer(bootstrap_servers=['broker:29092'], max_block_ms=5000)
+    curr_time = time.time()
+
+    # Looping data 1 minute
+    while True:
+        if time.time() > curr_time + 60: #1 minute
+            break
+        try:
+            res = get_data()
+            res = format_data(res)
+
+            producer.send('users_created', json.dumps(res).encode('utf-8'))
+        except Exception as e:
+            logging.error(f'An error occured: {e}')
+            continue
+
+# fungsi data streaming
+with DAG('user_automation',
+         default_args=default_args,
+         schedule_interval='@daily',
+         catchup=False) as dag:
+
+    streaming_task = PythonOperator(
+        task_id='stream_data_from_api',
+        python_callable=stream_data
+    )
